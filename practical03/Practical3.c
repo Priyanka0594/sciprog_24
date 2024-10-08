3b6eb00f76ad307fce97a2d83e56a0b709f39ab7
Priyanka Kaushal <priyanka.kaushal@ucdconnect.ie>
2024-10-08 18:37:04 +0100

    push downloaded files

--- practical03/Practical3.c
+++ practical03/Practical3.c
@@ -6,16 +6,27 @@ int main() {
     double a,sum = 0.0; // Starting point
     double b = M_PI / 3; // Ending point
     double step = (b - a) / (N - 1); // Calculate the step size
+	double diff;
 
     printf("Equidistant points between %f and %f:\n", a, b);
-    for (int i = 0; i < N; i++) {
+	sum= tan(a)+tan(b);
+    for (int i = 0; i < N; i++)
+   {
         double x = a + i * step; // Calculate each point
-        printf("x[%d] = %f\n", i, x);
-	sum = sum + 2*tan(x);
+	 printf("x[%d] = %f\n", i, x);
+	if (i>=1 & i<=11)
+	{
+	sum = sum + ( 2*tan(x));	
+	printf("Sum steps %f",sum);	
+}
     }
 
-sum= (sum+a+b)*((b-a)/(float)N);
+sum= (sum)*((b-a)/((float)2*(N)));
+printf("Sum  %f" , sum);
 
+printf("log 2 %lf",logf(2));
+diff= sum-logf(2);
+printf("differnece seen is %f",diff);
 
 
     return 0;
